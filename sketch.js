2e5482f71f36fa8949518d4fbf1c272bdd032e0d
HackerMan012 <89112161+HackerMan012@users.noreply.github.com>
2022-05-04 18:34:56 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var bg, bgimg;
 var sean, seanimg;
 var colm, colmimg;
+var pull, pullimg, slide, slideimg;
 
 function preload() {
   bgimg = loadImage("Picture1.webp");
@@ -13,13 +14,18 @@ function setup() {
   bg = createSprite(725, 360);
   bg.addImage(bgimg);
   bg.scale = 2.0;
-  bg.velocityX = -3;
+  
   //player
-  sean = createSprite(300, 300);
+  sean = createSprite(250, 300, 500, 1000);
   sean.shapeColor = "green";
   //Ai
   colm = createSprite(1200, 300);
   colm.shapeColor = "black";
+
+  //slider & draw
+  slider = createSprite(1400, 550, 50, 320);
+  pull = createSprite(1400, 685, 70, 70);
+  pull.shapeColor = "white";
 }
 
 function draw() {
@@ -27,26 +33,22 @@ function draw() {
   if(bg.x < 150) {
     bg.x = 725;
   }  
-  console.log(bg.x);
+  
 
-  if(keyIsDown(UP_ARROW)) {
-    sean.y -= 3;
+  if(keyIsDown(RIGHT_ARROW)) {
+    bg.x -= 3;
+    colm.x -=3;
   }
 
-  if(keyIsDown(DOWN_ARROW)) {
-    sean.y += 3;
+  if(keyIsDown(LEFT_ARROW)) {
+    bg.x += 3;
+    colm.x +=3;
   }
 
-  //Colm
-  movingColm();
+  //slider
+  
 
 
   drawSprites();
 }
-function movingColm() {
-  if(frameCount %60 === 0) {
-    colm.x = Math.round(random(500, 1300));
-    colm.y = Math.round(random(30, 660));
 
-  }
-}
